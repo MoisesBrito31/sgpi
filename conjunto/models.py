aaf0b53904f1dd50ab46acf056337ff90674b4c6
MoisesBrito31 <moisesbritofuentes@gmail.com>
2020-08-31 19:30:04 -0300

    conjunto

--- conjunto/models.py
+++ conjunto/models.py
@@ -30,7 +30,7 @@ class Componente(Base):
     nome = models.CharField('Componente', max_length=200)
     descricao = models.CharField('Descrição', max_length=200, blank=True)
     preco_compra = models.DecimalField('Preço Compra', decimal_places=2, max_digits=8)
-    preco_venda = models.DecimalField('Preço Venda', decimal_places=2, max_digits=8, blank=True)
+    preco_venda = models.DecimalField('Preço Venda', decimal_places=2, max_digits=8, default=0.00)
     fator = models.DecimalField('Fator de Conversão' ,decimal_places=4, max_digits=5 ,default=2.00)
     tipo = models.ForeignKey(TipoComponente, on_delete=models.CASCADE)
 
@@ -44,7 +44,7 @@ class Componente(Base):
 class Conjunto(Base):
     nome = models.CharField('Conjunto',max_length=200)
     tipo = models.ForeignKey(TipoConjunto, on_delete=models.CASCADE)
-    valor = models.DecimalField('Preço', max_digits=8, decimal_places=2,blank=True)
+    valor = models.DecimalField('Preço', max_digits=8, decimal_places=2, default=0.00)
     lista = models.ManyToManyField(Componente, through='Relacao')
 
     class Meta:
@@ -58,32 +58,29 @@ class Relacao(Base):
     componente = models.ForeignKey(Componente,blank=True, on_delete=models.CASCADE)
     Item = models.ForeignKey(Conjunto, blank=True, on_delete=models.CASCADE)
     quantidade = models.IntegerField('Quantidade')
-    valor = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    valor = models.DecimalField('Preço', decimal_places=2, max_digits=8, default=0.00)
 
     class Meta:
         verbose_name = 'Relação'
         verbose_name_plural = 'Realações'        
 
     def __str__(self):
-        return f'{self.componente.nome} x {quantidade}'
+        return f'{self.componente.nome} x {self.quantidade}'
 
-"""
+
 @receiver(pre_save, sender=Conjunto)
-def calcular(sender,instance,*args,**kargs):
-    try:
-        qtd = instance.relacao.quantidade
-        preco = instance.relacao.componente.preco_venda
-    except:
-        qtd = 0.00
-        preco = 0.00
-    instance.valor = qtd * preco
+def calcularConjunto(sender,instance,*args,**kargs):
+    valor_final = 0.00
+    for x in instance.relacao_set.all():
+        valor_final+=float(x.quantidade*x.componente.preco_venda)
+    instance.valor = valor_final
 
 @receiver(pre_save, sender=Relacao)
-def calcular(sender,instance,*args,**kargs):
+def calcularRelacao(sender,instance,*args,**kargs):
     instance.valor = instance.componente.preco_venda * instance.quantidade
 
 @receiver(pre_save, sender=Componente)
 def calcular(sender,instance,*args,**kargs):
     instance.preco_venda = instance.preco_compra * instance.fator
+    
 
-"""
