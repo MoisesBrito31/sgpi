171ed0e676bb5ddd7267d813721f64f19e52a909
MoisesBrito31 <moisesbritofuentes@gmail.com>
2020-08-30 23:23:02 -0300

    criação de cliente Model

---
+++ cliente/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from core.models import Base
+from item.models import Item
+
+
+class Cargo(Base):
+    nome = models.CharField('Cargo', max_length=20)
+    descricao = models.CharField('descrição', max_length=50, default='', blank=True)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self):
+        return self.nome
+
+
+class Responsavel(Base):
+    nome = models.CharField('Nome',max_length=50)
+    email = models.EmailField('E-Mail', max_length=60)
+    telefone = models.IntegerField('Telefone')
+    cargo = models.ForeignKey(Cargo, blank=True, on_delete=models.CASCADE)
+   
+
+    class Meta:
+        verbose_name = 'Responsável'
+        verbose_name_plural = 'Responsáveis'
+
+    def __str__(self):
+        return self.nome
+
+
+class Cliente(Base):
+    nome = models.CharField('Cliente', max_length=30, unique=True)
+    endereco = models.CharField('Endereço', max_length=50, blank=True)
+    cnpj = models.CharField('CNPJ', max_length=20, blank=True)
+    responsaveis = models.ManyToManyField(Responsavel,through='Relacao')
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+
+    def __str__(self):
+        return self.nome
+
+class Relacao(Base):
+    funcionario = models.ForeignKey(Responsavel, on_delete=models.CASCADE)
+    empresa = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Relação'
+        verbose_name_plural = 'Relações'
+
+
--- sgpi/settings.py
+++ sgpi/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'item',
+    'cliente',
     'stdimage',
     'bootstrap4',
 ]
